843154de53e13d5d0393e8a754ff72ec799d8060
Jacob Fromm <jacobfromm@gmail.com>
2020-07-22 20:41:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,8 +3,8 @@ def loop_message_five_times(message)
   5.times do puts message end
 end
 
-def loop_message_n_times
-  
+def loop_message_n_times(message, n)
+  n.times do puts message end
 end
 
 def output_array
